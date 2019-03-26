296e9d792de0ba24e593f0f3a336d1b627356982
Kaneki #Senpai <v.kaneki.senpai@gmail.com>
2019-03-26 18:02:21 +0100

    Add HttpStatus response

--- src/main/java/pl/aogiri/event/EventController.java
+++ src/main/java/pl/aogiri/event/EventController.java
@@ -11,22 +11,30 @@ import org.springframework.web.bind.annotation.*;
 
 import pl.aogiri.results.Result;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping(path = "/api")
 public class EventController {
-		
+
 	@Autowired
 	EventService eventService;
-	
+
 	@RequestMapping(method = RequestMethod.GET, path="events")
-	public @ResponseBody List<Event> getAllEvents(){		
-		return eventService.getAllEvents();		
+	public @ResponseBody List<Event> getAllEvents(HttpServletResponse response){
+		List<Event> events = eventService.getAllEvents();
+		if(events.size()==0)
+			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+		return events;
 	}
 
-	
+
 	@RequestMapping(method = RequestMethod.GET, path="events/{id}")
-	public @ResponseBody Event getEventById(@PathVariable Integer id) {
-		return eventService.getEventById(id);
+	public @ResponseBody Event getEventById(@PathVariable Integer id, HttpServletResponse response) {
+		Event event = eventService.getEventById(id);
+		if(event.getId()==null)
+			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+		return event;
 	}
 
 	//TODO need check how create event with user one-to-one
@@ -38,16 +46,19 @@ public class EventController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET, path="events/box")
-	public @ResponseBody List<Event> getBoxEvents(@RequestParam(value = "N", required = true) double N,@RequestParam(value = "S", required = true) double S,@RequestParam(value = "W", required = true) double W,@RequestParam(value = "E", required = true) double E, String date) throws ParseException {
-		return eventService.getBoxEvents(N, S, W, E, date);
+	public @ResponseBody List<Event> getBoxEvents(@RequestParam(value = "N", required = true) double N,@RequestParam(value = "S", required = true) double S,@RequestParam(value = "W", required = true) double W,@RequestParam(value = "E", required = true) double E, String date, HttpServletResponse response) throws ParseException {
+		List<Event> events = eventService.getBoxEvents(N, S, W, E, date);
+		if(events.size()==0)
+			response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+		return events;
 	}
 
 
 	//TODO update event after create any event :V
 	@RequestMapping(method = RequestMethod.PUT, path="events")
-	public @ResponseBody Result updateEvent(@RequestBody Map<String,String> body){		
-		return eventService.updateEvent(body);		
+	public @ResponseBody Result updateEvent(@RequestBody Map<String,String> body){
+		return eventService.updateEvent(body);
 	}
-	
+
 
 }
